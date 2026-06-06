1c34bfbeafd3b587860344b4a9b65628e6ca2459
unknown <mutheedavid12@gmail.com>
2026-06-06 19:14:29 +0300

    feat:learn new route concepts

--- app/main.py
+++ app/main.py
@@ -1,17 +1,63 @@
 from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
-
+from typing import Any
 
 
 app = FastAPI()
 
-
-@app.get("/shipment")
-def get_shipment():
-    return {
-        "content": "wooden table",
-        "status": "in transit"
+shipments = {
+    1234 : {
+        "weight": .9,
+        "content": "Glass door",
+        "status": "Placed" 
+    },
+    1235 : {
+        "weight": 1.2,
+        "content": "Wooden chair",
+        "status": "In transit"
+    },
+    1236 : {
+        "weight": 2.5,
+        "content": "Metal desk",
+        "status": "Delivered"
+    },
+    1237 : {
+        "weight": 0.5,
+        "content": "Plastic box",
+        "status": "Placed"
+    },
+    1238 : {
+        "weight": 3.0,
+        "content": "Bookshelf",
+        "status": "In transit"
+    },
+    1239 : {
+        "weight": 1.5,
+        "content": "Lamp",
+        "status": "Delivered"
+    },
+    1240 : {
+        "weight": 2.0,
+        "content": "Cabinet",
+        "status": "Placed"
     }
+}
+
+@app.get("/shipment/latest")
+def get_latest_shipment():
+    id = max(shipments.keys())
+    return shipments[id]
+
+
+@app.get("/shipment/{id}")
+def get_shipment(id: int) -> dict[str, Any]:
+
+    if id not in shipments:
+        return {"detail": "Given id is not in shipments"}
+
+    return shipments[id]
+
+
 
 @app.get("/scalar")
 def get_scalar_docs():
