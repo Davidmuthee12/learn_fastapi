7aa502e983bfcc706ec9f425453c5cc42f6b3368
unknown <mutheedavid12@gmail.com>
2026-06-09 14:15:21 +0300

    feat: learn adding primary key to tables

--- app/database.py
+++ app/database.py
@@ -8,28 +8,37 @@ cursor = connection.cursor()
 # 1.Create a table
 cursor.execute("""
             CREATE TABLE IF NOT EXISTS shipment (
-               id INTEGER,
+               id INTEGER PRIMARY KEY,
                content TEXT,
                weight REAL,
                status TEXT
             )
 """)
 
+# cursor.execute("DROP TABLE IF EXISTS shipment")
+# connection.commit()
+
 # 2. Add shipment data
 cursor.execute("""
     INSERT INTO shipment
-    VALUES (1243, 'Missile', 18.5, 'placed')
+    VALUES (1241, 'Missile', 18.5, 'placed')
 """)
 connection.commit()
 
 # 3. Read a shipment by id
-cursor.execute("""
-    SELECT * FROM shipment
-    WHERE id = 1241
-""")
-result = cursor.fetchall()
-print(result)
+# cursor.execute("""
+#     SELECT * FROM shipment
+#     WHERE id = 1241
+# """)
+# result = cursor.fetchall()
+# print(result)
 
+# 4. Delete a shipment
+# cursor.execute("""
+#     DELETE FROM shipment
+#     WHERE id = 1243
+# """)
+# connection.commit()
 
 # close the connection when done
 connection.close()
